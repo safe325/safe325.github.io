7bd2c912c6203ca07aff2b4017c427b478e7a04e
safe325 <safe9902@gmail.com>
2021-03-20 23:09:07 +0100

    scripts.js: if-else

--- 5/scripts.js
+++ 5/scripts.js
@@ -1,4 +1,27 @@
 let nevem = "Feri"
-let azEnKorom = 59
+let azEnKorom = 10
 console.log('A JavaScript is betöltött')
-console.log(azEnKorom)
+
+// egy egy egysoros komment
+/*
+tobb soros komment
+fejlesztoi eszkoztar a bongeszoben: Ctrl-Shift-I
+*/
+
+//relaciojelek: >, <, >=, <= 
+// == (egyenlo ertek), === (egyenlo ertek es tipus)
+if (azEnKorom >= 21) {
+    console.log('Legalabb 21 eves')
+} else if (azEnKorom >= 18) {
+  console.log('Nagykoru')
+} else {
+    console.log('Nem nagykoru')
+}
+
+let masikKor = 18
+
+if (masikKorom === 18) {
+    console.log('Pont tizennyolc')
+}
+
+console.log('Ez itt a vege')
